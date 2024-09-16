00e6cf6492e25a5989d08381154018a1dec6a70d
Benjamin Kober <benko@kober-systems.com>
2024-09-16 18:43:39 +0300

    refactoring to functions

--- crates/gravity/tests/test_kv_store.rs
+++ crates/gravity/tests/test_kv_store.rs
@@ -7,7 +7,7 @@ fn create_a_node_in_empty_store() -> Result<(), Error> {
 
   graph.create_node(uuid!(EXAMPLE_UUID), &PROPERTY_EMPTY.to_vec())?;
 
-  let mut store = graph.into_kv().get_inner();
+  let mut store = get_kv_store(graph);
   check_string(
     store.remove(&format!("nodes/{}", EXAMPLE_UUID)),
     &format!(
@@ -35,10 +35,15 @@ const EXAMPLE_UUID : &str = "a1a2a3a4-b1b2-c1c2-d1d2-d3d4d5d6d7d8";
 const PROPERTY_EMPTY : &[u8] = "".as_bytes();
 const PROPERTY_EMPTY_ID: &str = "E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855";
 
-fn create_empty_graph() -> kv_graph_store::KvGraphStore::<Vec<u8>, mem_kv_store::MemoryKvStore, mem_kv_store::Error> {
+fn create_empty_graph() -> GStore {
   let kv = mem_kv_store::MemoryKvStore::default();
   kv_graph_store::KvGraphStore::from_kv(kv)
 }
 
+fn get_kv_store(graph: GStore) -> std::collections::BTreeMap<String, Vec<u8>> {
+  graph.into_kv().get_inner()
+}
+
 type Error = kv_graph_store::Error<mem_kv_store::Error>;
+type GStore = kv_graph_store::KvGraphStore::<Vec<u8>, mem_kv_store::MemoryKvStore, mem_kv_store::Error>;
 
