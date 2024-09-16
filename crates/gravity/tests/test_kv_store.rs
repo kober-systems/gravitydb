4e39cd0713b20026f3cf2e6b9671af413b4e827c
Benjamin Kober <benko@kober-systems.com>
2024-09-16 17:16:56 +0300

    refactoring tests

--- crates/gravity/tests/test_kv_store.rs
+++ crates/gravity/tests/test_kv_store.rs
@@ -6,20 +6,18 @@ fn create_a_node_in_empty_store() -> Result<(), Error> {
   let kv = mem_kv_store::MemoryKvStore::default();
   let mut graph = kv_graph_store::KvGraphStore::<Vec<u8>, mem_kv_store::MemoryKvStore, mem_kv_store::Error>::from_kv(kv);
 
-  graph.create_node(uuid!("a1a2a3a4-b1b2-c1c2-d1d2-d3d4d5d6d7d8"), &"".as_bytes().to_vec())?;
+  graph.create_node(uuid!(EXAMPLE_UUID), &"".as_bytes().to_vec())?;
 
   let mut store = graph.into_kv().get_inner();
   check_string(
-    store.remove("nodes/a1a2a3a4-b1b2-c1c2-d1d2-d3d4d5d6d7d8"),
-    "{\"id\":\"a1a2a3a4-b1b2-c1c2-d1d2-d3d4d5d6d7d8\",\"properties\":\"E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855\",\"incoming\":[],\"outgoing\":[]}"
+    store.remove(&format!("nodes/{}", EXAMPLE_UUID)),
+    &format!("{{\"id\":\"{}\",\"properties\":\"E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855\",\"incoming\":[],\"outgoing\":[]}}", EXAMPLE_UUID)
   );
   check_string(
     store.remove("props/E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855"),
     ""
   );
 
-  //let keys: Vec<_> = store.keys().cloned().collect();
-  //assert_eq!(keys[0], "".to_string());
   Ok(assert_eq!(store.len(), 0))
 }
 
@@ -29,6 +27,8 @@ fn check_string(left: Option<Vec<u8>>, right: &str) {
 
   assert_eq!(formatted, right.to_string())
 }
+
+const EXAMPLE_UUID : &str = "a1a2a3a4-b1b2-c1c2-d1d2-d3d4d5d6d7d8";
 
 type Error = kv_graph_store::Error<mem_kv_store::Error>;
 
