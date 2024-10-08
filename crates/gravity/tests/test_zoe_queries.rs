3dc9498bc62f06991b680c7db6caa77698501dbe
Benjamin Kober <benko@kober-systems.com>
2024-10-08 13:19:38 +0300

    added album please please me

--- crates/gravity/tests/test_zoe_queries.rs
+++ crates/gravity/tests/test_zoe_queries.rs
@@ -27,6 +27,36 @@ fn create_simple_beatles_graph() -> Result<GStore, Error> {
   let acoustic_guitar = g.create_node(Uuid::new_v4(), &Instrument("Acoustic Guitar".to_string()))?;
   let drums = g.create_node(Uuid::new_v4(), &Instrument("Drums".to_string()))?;
 
+  let album = g.create_node(Uuid::new_v4(), &Album("Please Please Me".to_string()))?;
+  let i_saw_her_standing_there = g.create_node(Uuid::new_v4(), &Song("I Saw Her Standing There".to_string()))?;
+  g.create_edge(album, i_saw_her_standing_there, &Features)?;
+  let misery = g.create_node(Uuid::new_v4(), &Song("Misery".to_string()))?;
+  g.create_edge(album, misery, &Features)?;
+  let anna_go_to_him = g.create_node(Uuid::new_v4(), &Song("Anna (Go To Him)".to_string()))?;
+  g.create_edge(album, anna_go_to_him, &Features)?;
+  let chains = g.create_node(Uuid::new_v4(), &Song("Chains".to_string()))?;
+  g.create_edge(album, chains, &Features)?;
+  let boys = g.create_node(Uuid::new_v4(), &Song("Boys".to_string()))?;
+  g.create_edge(album, boys, &Features)?;
+  let ask_me_why = g.create_node(Uuid::new_v4(), &Song("Ask Me Why".to_string()))?;
+  g.create_edge(album, ask_me_why, &Features)?;
+  let please_please_me = g.create_node(Uuid::new_v4(), &Song("Please Please Me".to_string()))?;
+  g.create_edge(album, please_please_me, &Features)?;
+  let love_me_do = g.create_node(Uuid::new_v4(), &Song("Love Me Do".to_string()))?;
+  g.create_edge(album, love_me_do, &Features)?;
+  let ps_i_love_you = g.create_node(Uuid::new_v4(), &Song("PS I Love You".to_string()))?;
+  g.create_edge(album, ps_i_love_you, &Features)?;
+  let baby_its_you = g.create_node(Uuid::new_v4(), &Song("Baby It’s You".to_string()))?;
+  g.create_edge(album, baby_its_you, &Features)?;
+  let do_you_want_to_know_a_secret = g.create_node(Uuid::new_v4(), &Song("Do You Want To Know A Secret".to_string()))?;
+  g.create_edge(album, do_you_want_to_know_a_secret, &Features)?;
+  let a_taste_of_honey = g.create_node(Uuid::new_v4(), &Song("A Taste Of Honey".to_string()))?;
+  g.create_edge(album, a_taste_of_honey, &Features)?;
+  let theres_a_place = g.create_node(Uuid::new_v4(), &Song("There’s A Place".to_string()))?;
+  g.create_edge(album, theres_a_place, &Features)?;
+  let twist_and_shout = g.create_node(Uuid::new_v4(), &Song("Twist And Shout".to_string()))?;
+  g.create_edge(album, twist_and_shout, &Features)?;
+
   let abbey_road = g.create_node(Uuid::new_v4(), &Album("Abbey Road".to_string()))?;
   let yesterday = g.create_node(Uuid::new_v4(), &Song("Yesterday".to_string()))?;
   g.create_edge(john, yesterday, &Wrote)?;
