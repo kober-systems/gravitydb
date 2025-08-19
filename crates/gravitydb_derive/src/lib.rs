119b499579bca085dda08bfb1cb6c3aa4d8860bb
Benjamin Kober <benko@kober-systems.com>
2025-08-19 13:22:12 +0300

    improved error messages

--- crates/gravitydb_derive/src/lib.rs
+++ crates/gravitydb_derive/src/lib.rs
@@ -51,7 +51,7 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
                         #(#types)*
                     }
                   }
-                  _ => unimplemented!("{}", attr_name)
+                  _ => unimplemented!("attribute '{}' not supported", attr_name)
                 }
               },
               _ => unimplemented!()
